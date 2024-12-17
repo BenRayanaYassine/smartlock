756d3073e76f7b59e5b0ff37a1e79da48b98a2e7
BenRayanaYassine <brryanayassine4@gmail.com>
2024-12-17 16:03:19 +0100

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lock/FirstScreen.dart';
+import 'package:lock/login.dart'; // Import the LoginScreen
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/login', // Start the app at the LoginScreen
+      routes: {
+        '/login': (context) => LoginScreen(), // Define the login route
+        '/home': (context) => FirstScreen(), // Define the home route
+      },
+    );
+  }
+}
